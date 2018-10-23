cd30251ec5f6a834b23dc1c654e4078ecde13cd5
wsylt <yydzhangyihan@163.com>
2018-10-23 10:53:10 +0800

    1023_3

--- c/lab2/ex3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("number square cube\n");
-	for(int i; i <= 10; i++){
-		printf("%d  %d  %d\n", i, i * i, i * i * i);
-	}
-}
--- c/lab2/ex4_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int t;
-	int h, min, sec;
-	scanf("%d:%d:%d", &h, &min, &sec);
-	t = h * 3600 + min * 60 + sec;
-	printf("%d\n", t);
-}
